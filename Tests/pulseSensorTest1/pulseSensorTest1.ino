6bfa7bc74dd785b2195933b11a6db7dcb74b6ef3
bhgt <bhagat.nagi@gmail.com>
2020-02-01 10:06:04 +0530

    Updated Notification Tests and added CommTest (Working)

--- Tests/pulseSensorTest1/pulseSensorTest1.ino
+++ Tests/pulseSensorTest1/pulseSensorTest1.ino
@@ -1,7 +1,26 @@
+int lastVal = 0;
+int currVal = 0;
+int lastDif = 0;
+int currDif = 0;
+
 void setup() {
   Serial.begin(9600);
+  lastVal = 0;
+  currVal = 0;
+  lastDif = 0;
+  currDif = 0;
 }
 
 void loop() {
-  Serial.println(analogRead(A0));
+  lastVal = currVal;
+  currVal = analogRead(A0);
+  currDif = currVal-lastVal;
+  if(currDif>5)
+    if((currDif-lastDif)>0)
+      Serial.println((currDif-lastDif));
+    else
+      Serial.println(0);
+  else
+    Serial.println(0);
+  delay(100);
 }
